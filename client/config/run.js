445065ac54de93d53fc370ee7279ffe1b664be5b
Jessica Leivers <sarahjessicaleivers@gmail.com>
2015-05-01 00:46:20 -0700

    bootstrap material design added

--- client/config/run.js
+++ client/config/run.js
@@ -4,4 +4,6 @@ angular.module('photoAlbums')
 .run(function($rootScope, $window, $firebaseAuth, firebaseUrl){
   $rootScope.fbRoot = new $window.Firebase(firebaseUrl);
   $rootScope.afAuth = $firebaseAuth($rootScope.fbRoot);
+
+  $.material.init();
 });
